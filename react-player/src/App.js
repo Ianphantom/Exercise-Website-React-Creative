19df1ce979910bfcaa4967dbd8ff83ef66a6bdb8
Ianphantom <ian25yola@gmail.com>
2022-06-30 18:06:52 +0700

    initial template music Project

---
+++ react-player/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import Player from './components/Player';
+import Song from "./components/Song";
+
+import './styles/app.scss' 
+
+import data from './util';
+
+function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  return (
+    <div className="App">
+      <h1>Music Player</h1>
+      <Song currentSong={currentSong}/>
+      <Player currentSong={currentSong}/>
+    </div>
+  );
+}
+
+export default App;

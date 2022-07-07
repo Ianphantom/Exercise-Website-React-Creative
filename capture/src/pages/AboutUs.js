f865daaba9cdb4f9a04efd4cb5425bbe2b2afbe2
Ianphantom <ian25yola@gmail.com>
2022-07-08 01:38:41 +0700

    Global style dan AboutSection style

--- capture/src/pages/AboutUs.js
+++ capture/src/pages/AboutUs.js
@@ -4,13 +4,13 @@ import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-    return(
-        <div>
-            <AboutSection/>
-            <ServicesSection/>
-            <FaqSection/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </div>
+  );
+};
 
 export default AboutUs;

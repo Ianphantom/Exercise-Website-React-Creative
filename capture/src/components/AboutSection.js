2820c6cfee0b7c04d8b4950d90486ce95621020c
Ianphantom <ian25yola@gmail.com>
2022-07-09 16:06:42 +0700

    About us Animation

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
 
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -10,25 +11,29 @@ const AboutSection = () => {
       <Description>
         <motion.div className='title'>
           <Hide>
-            <motion.h2>We Work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We Work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               Your <span>Dream</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. we
           have proffessional with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt='Guy with a camera' />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt='Guy with a camera'
+        />
       </Image>
     </About>
   );

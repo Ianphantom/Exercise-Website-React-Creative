fbe37cc571450cd7273204c75a871a0a03f6a747
Ianphantom <ian25yola@gmail.com>
2022-07-14 23:09:20 +0700

    Menggunakan data satu persatu dari games dimasukkan dalam card dan diberikan style

--- ignite/src/App.js
+++ ignite/src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/home";
 function App() {
   return (
     <div className='App'>
-      <h1>Ignite</h1>
       <Home />
     </div>
   );
--- ignite/src/pages/home.js
+++ ignite/src/pages/home.js
@@ -1,13 +1,49 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+import Game from "../components/Game";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
-  return <div></div>;
+  }, [dispatch]);
+
+  const { popular, newGames, upComing } = useSelector((state) => state.games);
+  //   console.log(newGames);
+  return (
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upComing.map((data) => (
+          <Game
+            name={data.name}
+            released={data.released}
+            id={data.id}
+            image={data.background_image}
+            key={data.id}
+          />
+        ))}
+      </Games>
+    </GameList>
+  );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;

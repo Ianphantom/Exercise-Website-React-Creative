3db96c4bde2076312c7148893737e9cddf19e02f
Ianphantom <ian25yola@gmail.com>
2022-07-17 23:44:14 +0700

    menambahkan gambar bintang sesaui dengan rating games nya

--- ignite/src/components/GameDetail.js
+++ ignite/src/components/GameDetail.js
@@ -10,6 +10,8 @@ import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
@@ -39,6 +41,21 @@ const GameDetail = ({ pathId }) => {
   };
 
   const { screen, game, isLoading } = useSelector((state) => state.detail);
+
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      console.log(i);
+      if (i <= rating) {
+        stars.push(<img alt='star' key={i} src={starFull} />);
+      } else {
+        stars.push(<img alt='star' key={i} src={starEmpty} />);
+      }
+    }
+    return stars;
+  };
+
   return (
     <div>
       {!isLoading && (
@@ -48,6 +65,7 @@ const GameDetail = ({ pathId }) => {
               <div className='rating'>
                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating : {game.rating}</p>
+                {getStars()}
               </div>
               <Info>
                 <h3>Platform</h3>
@@ -126,6 +144,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`

e7e462ff156f3ee1266901ae06de999378623262
Ianphantom <ian25yola@gmail.com>
2022-07-17 23:17:07 +0700

    Menambahkan Icon Platform yang bisa digunakan pada setiap game

--- ignite/src/components/GameDetail.js
+++ ignite/src/components/GameDetail.js
@@ -4,6 +4,12 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { smallImage } from "../util";
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
@@ -14,6 +20,24 @@ const GameDetail = ({ pathId }) => {
       navigate("/");
     }
   };
+
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+
   const { screen, game, isLoading } = useSelector((state) => state.detail);
   return (
     <div>
@@ -29,7 +53,11 @@ const GameDetail = ({ pathId }) => {
                 <h3>Platform</h3>
                 <Platforms>
                   {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                      alt={data.platform.name}
+                    />
                   ))}
                 </Platforms>
               </Info>
@@ -68,6 +96,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 5;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
@@ -87,6 +116,7 @@ const Detail = styled(motion.div)`
   background: white;
   left: 10%;
   color: black;
+  z-index: 10;
   img {
     width: 100%;
   }
